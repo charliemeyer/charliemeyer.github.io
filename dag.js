abc6863173ab6fcc14f7e1fc819c84eee649045a
Charlie Meyer <cmey63@gmail.com>
2016-12-30 01:57:26 -0500

    add dag background, change picture, add new projects, kill colors

---
+++ dag.js
@@ -0,0 +1,36 @@
+//////////////////////////////////////////////////////////////
+////////////////////// Global Resources //////////////////////
+//////////////////////////////////////////////////////////////
+
+// Set up canvas and popups
+var paper_w = $("#canvas").width();
+var paper_h = $("#canvas").height();
+var canvas = document.getElementById("canvas");
+var paper = Raphael(canvas, paper_w, paper_h);
+
+var num_points = 10;
+var points = [];
+
+$(document).ready(function() {
+    for (var i = 0; i < num_points; i++) {
+        points.push(draw_point(Math.round(Math.random()*paper_w), Math.round(Math.random()*paper_h), "#000000", 4));
+    }
+    for (var i = 0; i < num_points-1; i++) {
+        draw_path(points[i], points[i+1], "#000000", 1);
+    }
+
+    $("#content").fadeIn(50);
+});
+
+// Draw point on canvas at x y with given color and radius
+function draw_point(x, y, color, radius) {
+    var new_circ = paper.circle(x, y, radius).attr({fill: color, stroke: "#FFFFFF"});
+    return new_circ;
+}
+
+function draw_path(p1, p2, color, stroke_width) {
+    p = paper.path(["M", p1.attr("cx"), p1.attr("cy"), "L", p2.attr("cx"), p2.attr("cy")]);
+    p.attr({stroke:color,"stroke-width":stroke_width});
+    p.toBack();
+    return p;
+}
