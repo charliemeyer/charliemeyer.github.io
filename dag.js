1e60749e1df3d9b9ce1ec92388c155a8dc12c7b1
Charlie Meyer <cmey63@gmail.com>
2017-01-04 22:35:16 -0500

    better looking background graph

--- dag.js
+++ dag.js
@@ -8,7 +8,7 @@ var paper_h = $("#canvas").height();
 var canvas = document.getElementById("canvas");
 var paper = Raphael(canvas, paper_w, paper_h);
 
-var num_points = 10;
+var num_points = 12;
 var points = [];
 
 $(document).ready(function() {
@@ -22,11 +22,32 @@ function generate_background() {
     var paper_h = $("#canvas").height();
     paper.clear();
     points = [];
-    for (var i = 0; i < num_points; i++) {
-        points.push(draw_point(Math.round(Math.random()*paper_w), Math.round(Math.random()*paper_h), "#000000", 5));
+    for (var i = 0; i < 2; i++) {
+        for (var j = 0; j < 2; j++) {
+            for (var k = 0; k < num_points/4; k++) {
+                points.push(draw_point(Math.round((paper_w/2)*i+Math.random()*paper_w/2), Math.round((paper_h/2)*j+Math.random()*paper_h/2), "#000000", 5));
+            }
+        }        
     }
-    for (var i = 0; i < num_points-1; i++) {
-        draw_path(points[i], points[i+1], "#000000", 1);
+    outgoing_edges = [];
+    for (var i = 0; i < num_points; i++) {
+        outgoing_for_me = [];
+        for (var j = 0; j < num_points; j++) {
+            if (i != j) {
+                if (Math.random() > .95) {
+                    outgoing_for_me.push(j);
+                    draw_path(points[i], points[j], "#000000", 1);
+                }
+            }
+        }
+        if (outgoing_for_me.length == 0) {
+            my_edge = Math.round(Math.random(num_points - 1));
+            if (my_edge < i) {
+                draw_path(points[i], points[my_edge], "#000000", 1);
+            } else {
+                draw_path(points[i], points[my_edge + 1], "#000000", 1);
+            }
+        }
     }
 }
 
@@ -41,4 +62,8 @@ function draw_path(p1, p2, color, stroke_width) {
     p.attr({stroke:color,"stroke-width":stroke_width});
     p.toBack();
     return p;
+}
+
+function dist(p1, p2) {
+    return Math.round(Math.sqrt( (p1.attr("cx")-p2.attr("cx"))*(p1.attr("cx")-p2.attr("cx"))+(p1.attr("cy")-p2.attr("cy"))*(p1.attr("cy")-p2.attr("cy"))));
 }
